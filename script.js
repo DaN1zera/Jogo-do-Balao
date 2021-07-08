1158e8a26317b027dc50e808551b0b2b3592aa91
DaN1zera <daniielreis@live.com>
2021-07-08 17:39:30 -0300

    Nomes de variáveis trocados e declaração mudada para 'let'

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // 1 Criar o balao
 // 2 Estourar o balao
 // 3 Carregar o jogo
-var total = 0;
-var totalBalaoF = 0;
-var totalBalao = 0;
-var pontos = 1;
-var parar = false;
-var chances = 3;
-var balaoSumiu = 0;
-var ativarPerdeu = false;
+let total = 0;
+let idBalao = 0;
+let idBalaoRemover = 0;
+let pontos = 1;
+let parar = false;
+let chances = 3;
+let balaoSumiu = 0;
+let atiletPerdeu = false;
 
 function criarBalao() {
     if (parar == false) {
-        var balao = document.createElement("div");
+        let balao = document.createElement("div");
         balao.setAttribute("class", "balao");
-        balao.setAttribute("id", "" + totalBalaoF);
-        var x = Math.floor(Math.random() * 600);
-        var y = Math.floor(Math.random() * 400);
+        balao.setAttribute("id", "" + idBalao);
+        let x = Math.floor(Math.random() * 600);
+        let y = Math.floor(Math.random() * 400);
         balao.setAttribute(
             "style",
             "left:" +
@@ -28,26 +28,25 @@ function criarBalao() {
         balao.setAttribute("onclick", "estourar(this)");
         document.body.appendChild(balao);
         setTimeout(function () {
-            const excluirBalao = document.getElementById(totalBalao++);
+            const excluirBalao = document.getElementById(idBalaoRemover++);
             excluirBalao.parentNode.removeChild(excluirBalao);
             balaoSumiu++;
         }, 1550);
-        totalBalaoF++;
+        idBalao++;
     }
 }
 
 function estourar(objeto) {
     document.body.removeChild(objeto);
     total++;
-    var score = document.getElementById("total");
+    let score = document.getElementById("total");
     score.innerHTML = "Balões estourados: " + total;
 }
 
-function perdeu(objeto) {
-    if (ativarPerdeu == true) {
-        console.log(objeto);
+function perdeu() {
+    if (atiletPerdeu == true) {
         chances--;
-        erros = document.getElementById("erros");
+        let erros = document.getElementById("erros");
         erros.innerHTML = "Chances disponíveis: " + chances;
         pontos++;
         if (pontos == 4) {
@@ -56,14 +55,14 @@ function perdeu(objeto) {
             alert("Você perdeu");
             erros.innerHTML = "Chances disponíveis: 3";
         }
-    } else if (ativarPerdeu == false) {
+    } else if (atiletPerdeu == false) {
         return false;
     }
 }
 
 function removerBalao() {
     setInterval(function loopDerrota() {
-        for (var i = 0; i < 1; i++) {
+        for (let i = 0; i < 1; i++) {
             if (balaoSumiu == 10) {
                 window.location.reload();
                 alert("Você perdeu");
@@ -79,7 +78,7 @@ function carregarJogo() {
     chances = 3;
     totalBalao = 0;
     total = 0;
-    ativarPerdeu = true;
+    atiletPerdeu = true;
     setInterval(criarBalao, 400);
     removerBalao();
 }
